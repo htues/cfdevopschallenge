0ae979ca3d42ce341981330bf1d7c175b00749af
Herbert Eduardo Fernandez Tamayo <81975375+htues@users.noreply.github.com>
2024-08-27 18:28:40 +0000

    first version of hours component

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,14 @@
-// Placeholder to host a component
+import React from 'react';
+
+const OfficeHours = () => {
+  return (
+    <div>
+      <h2>Office Hours</h2>
+      <p>Monday to Friday: from 10:00 to 16:00 hours</p>
+      <p>Weekends: from 9:00 to 20:00 hours</p>
+      <p>React component by: Herbert Tamayo :-) </p>
+    </div>
+  );
+};
+
+export default OfficeHours;// Placeholder to host a component
